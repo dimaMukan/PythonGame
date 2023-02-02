42bdd4bb5013aa5db66a02687bc7940f3da73849
Олег <olegkovalets1112003@knu.ua>
2023-02-02 14:06:40 +0200

    Emojis

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ import random
 import time
 import emoji
 
-print("hello")
+rock = emoji.emojize(":rock:")
+paper = emoji.emojize(":page_facing_up:")
+scissors = emoji.emojize(":scissors:")
+lizard = emoji.emojize(":lizard:")
+spock = emoji.emojize(":vulcan_salute:")
+
+
+items = {1: "rock", 2: "paper", 3: "scissors", 4: "lizard", 5: "spock"}
+emoji_items = {1: rock, 2: paper, 3: scissors, 4: lizard, 5: spock}
