875c95730f38debfeb7a17839ac21b505d5b225b
dimaMukan <ds.mukan@gmail.com>
2023-02-09 00:24:38 +0100

    Dry principe + some defs + new branch

--- lab_1/lab_1.py
+++ lab_1/lab_1.py
@@ -11,56 +11,56 @@ spock = emoji.emojize(":vulcan_salute:")
 items = {1: "rock", 2: "paper", 3: "scissors", 4: "lizard", 5: "spock"}
 emoji_items = {1: rock, 2: paper, 3: scissors, 4: lizard, 5: spock}
 
+def win():
+    print("YOU WIN!")
+def lose():
+    print(f"YOU LOSE!")
+
+
 def game_core(bot_item, user_item):
     print(emoji_items[user_item] + "\t" + emoji.emojize(":right_arrow:") + "\t" + emoji_items[bot_item])
+
     if user_item == bot_item:
         print("TIE! MOVE TO NEXT ROUND!")
         game()
+
     if user_item == 1:
-        if bot_item == 2:
-            print(f"YOU LOSE!")
-        elif bot_item == 3:
-            print("WIN!")
-        elif bot_item == 4:
-            print("WIN!")
-        elif bot_item == 5:
-            print("LOSE!")
+        if bot_item == 3 or 4:
+            win()
+        else:
+            lose()
+
     if user_item == 2:
-        if bot_item == 1:
-            print("WIN!")
-        elif bot_item == 3:
-            print("LOSE!")
-        elif bot_item == 4:
-            print("LOSE!")
-        elif bot_item == 5:
-            print("WIN!")
+        if bot_item == 1 or 5:
+            win()
+        else:
+            lose()
+
     if user_item == 3:
-        if bot_item == 1:
-            print("LOSE!")
-        elif bot_item == 2:
-            print("WIN!")
-        elif bot_item == 4:
-            print("WIN!")
-        elif bot_item == 5:
-            print("LOSE!")
+        if bot_item == 2 or 4:
+            win()
+        else:
+            lose()
+
     if user_item == 4:
         if bot_item == 1:
-            print("LOSE!")
+            lose()
         elif bot_item == 2:
-            print("WIN!")
+            win()
         elif bot_item == 3:
-            print("LOSE!")
+            lose()
         elif bot_item == 5:
-            print("WIN!")
+            win()
+
     if user_item == 5:
         if bot_item == 1:
-            print("WIN!")
+            win()
         elif bot_item == 2:
-            print("LOSE!")
+            lose()
         elif bot_item == 3:
-            print("WIN!")
+            win()
         elif bot_item == 4:
-            print("LOSE!")
+            lose()
 
 
 def game():
